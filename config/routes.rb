92021be922069e2f63e729ade7ca620c08f9dfba
trangvuuet <trangvuuet@gmail.com>
2016-04-02 08:55:02 +0000

    Added rails_12factor gem and updated Gemfile.lock

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :comments
   get 'pages/info'
 
-  #root :to => redirect('/ideas')
+  root :to => redirect('/ideas')
   resources :ideas
   
   #get "trang" => "ideas#index"
