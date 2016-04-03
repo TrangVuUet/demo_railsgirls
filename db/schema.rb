4bccaf39559e82cd807b4878f397bfaa523946a4
trangvuuet <trangvuuet@gmail.com>
2016-04-03 03:08:17 +0000

    add picture to cmt

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402080314) do
+ActiveRecord::Schema.define(version: 20160403030713) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "user_name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160402080314) do
     t.integer  "idea_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "like_count"
+    t.string   "picture"
   end
 
   create_table "ideas", force: :cascade do |t|
