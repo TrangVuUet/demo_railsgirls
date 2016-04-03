13074af870e27bb3758e79fe2c4846e5eaf269fe
trangvuuet <trangvuuet@gmail.com>
2016-04-03 03:28:29 +0000

    after create and show picture of cmt

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
     belongs_to :idea
-    validates_presence_of :user_name, :body #validation for a commnet
+    
+    #validation for a commnetrails generate migration AddReplyIdToComments reply_id:integer
+    validates_presence_of :user_name, :body
+    mount_uploader :picture, PictureUploader
 end
+
